d72bd7debe4adcb29a91625dac314b846c8a18a0
jrpolesi <josericardo.kremer@gmail.com>
2022-01-08 18:09:33 -0300

    Add footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,6 +1,26 @@
-import './style.scss'
+import styles from './style.module.scss'
 
-export function Home(){
-
+export function Footer() {
+  return (
+    <>
+      <footer className={styles.footer}>
+        <div className='container'>
+          <nav className={styles.footer__nav}>
+            <h4>NEXT STORE</h4>
+            <ul>
+              <li>Contact Us</li>
+              <li>FAQ</li>
+              <li>Products</li>
+            </ul>
+          </nav>
+        </div>
+      </footer>
+      <div className={styles.footer__bottom}>
+        <div className='container'>
+          ©2021 Next Store
+        </div>
+      </div>
+    </>
+  )
 }
 
