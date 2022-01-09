a5e01ac0fea02d6dc1d60d0e03d4e6fe10d2ee12
jrpolesi <josericardo.kremer@gmail.com>
2022-01-09 16:08:26 -0300

    Add star icons on Product component

--- src/components/Product/index.jsx
+++ src/components/Product/index.jsx
@@ -1,12 +1,45 @@
+import { useEffect, useState } from 'react'
 import styles from './style.module.scss'
+import Image from 'next/image'
+
+import starIcon from '../../assets/images/star-icon.svg'
+import semiStarIcon from '../../assets/images/semistar-icon.svg'
 
 export function Product({ product, ...rest }) {
+  const [stars, setStars] = useState(null)
+
+  useEffect(() => {
+    const fullStars = Math.floor(product.rating.rate)
+    const hasRest = !Number.isInteger(product.rating.rate)
+    let stars = []
+
+    for (let i = 0; i < fullStars; i++) {
+      stars.push(
+        <div className={styles.star}>
+          <Image src={starIcon} alt='star' />
+        </div>
+      )
+    }
+
+    if (hasRest) {
+      stars.push(
+        <div className={styles.product__semistar}>
+          <Image src={semiStarIcon} alt='star' />
+        </div>
+      )
+    }
+
+    setStars(stars)
+  }, [])
+
   return (
     <div className='container'>
       <section className={styles.product}>
+
         <div className={styles.product__imageWraper}>
           <img src={product.image} alt={product.title} />
         </div>
+
         <div className={styles.product__info}>
           <div className={styles.product__titleContainer}>
             <h1 className={styles.product__title}>
@@ -14,12 +47,25 @@ export function Product({ product, ...rest }) {
               <span className={styles.product__id}>.000{product.id}</span>
             </h1>
           </div>
+
           <div className={styles.product__category}>{product.category}</div>
+
+          <div className={styles.product__imageWraper__mobile}>
+            <img src={product.image} alt={product.title} />
+          </div>
+
           <p className={styles.product__description}>{product.description}</p>
+
           <span className={styles.product__price}>U$ {product.price.toFixed(2)}</span>
+
           <button className={styles.product__button}>Add to cart</button>
-          <span>{product.rating.rate}</span>
+
+          <div className={styles.product__stars}>
+            {stars}
+            <span>{product.rating.rate} ({product.rating.count})</span>
+          </div>
         </div>
+
       </section>
     </div>
   )
