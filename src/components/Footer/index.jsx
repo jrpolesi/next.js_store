f0808c7709a7f8779aa980be61c07496994f5a17
jrpolesi <josericardo.kremer@gmail.com>
2022-01-09 12:37:15 -0300

    Fixed footer on bottom

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -3,8 +3,8 @@ import Link from 'next/link'
 
 export function Footer() {
   return (
-    <>
-      <footer className={styles.footer}>
+    <footer className={styles.footer}>
+      <div className={styles.footer__top}>
         <div className='container'>
           <nav className={styles.footer__nav}>
             <h4>NEXT STORE</h4>
@@ -19,13 +19,13 @@ export function Footer() {
             </ul>
           </nav>
         </div>
-      </footer>
+      </div>
       <div className={styles.footer__bottom}>
         <div className='container'>
           ©2021 Next Store
         </div>
       </div>
-    </>
+    </footer>
   )
 }
 
